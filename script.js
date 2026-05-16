a578de3a9030957b19f21bf80ae9a10510691587
HussainUsmani <hussainusmani216@gmail.com>
2026-05-16 19:05:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    // window.addEventListener('load') tab chalta hai jab images aur saara content load ho chuka ho
+    window.addEventListener('load', function() {
+        const preloader = document.getElementById('preloader');
+        // Preloader ko hide karne ke liye class add kar rahe hain
+        preloader.classList.add('preloader-hidden');
+    });
